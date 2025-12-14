9885e1c04ee2d451d871b26e494249bba91ae8fa
rickynation6 <rickynation25@gmail.com>
2025-12-14 04:44:19 +0100

    Update menu-new.js

--- plugins/menu-new.js
+++ plugins/menu-new.js
@@ -39,7 +39,7 @@ cmd({
             forwardingScore: 999,
             isForwarded: true,
             forwardedNewsletterMessageInfo: {
-                newsletterJid: '120363348739987203@newsletter',
+                newsletterJid: '248760697393182@newsletter',
                 newsletterName: config.Ricky-musky,
                 serverMessageId: 143
             }
